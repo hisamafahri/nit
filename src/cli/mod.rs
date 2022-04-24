390ff6d1dc832d899ac6ccbf9020ece4a28397ab
Hisam Fahri <me@hisamafahri.com>
2022-04-25 06:08:06 +0700

    feat(src): refactor cli struct

---
+++ src/cli/mod.rs
@@ -0,0 +1,27 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[clap(name = "nit")]
+#[clap(author = "Hisam Fahri <me@hisamafahri.com>")]
+#[clap(version = "0.1.0")]
+#[clap(about = "nit helps you wrap your git commands")]
+#[clap(long_about = "Wrap your git commands with nit to make your workflow faster and more consistent")]
+#[clap(propagate_version = true)]
+
+pub struct Cli {
+    #[clap(subcommand)]
+    pub command: Commands,
+}
+
+#[derive(Subcommand)]
+pub enum Commands {
+    /// Commit staged changes on the current working directory
+    Commit(Commit),
+}
+
+#[derive(Parser)]
+pub struct Commit {
+    /// Commit all changes (staged & unstaged changes) on the current working directory
+    #[clap(short, long)]
+    pub all: bool, 
+}
--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,16 @@
-use clap::{Parser, Subcommand};
-
-#[derive(Parser)]
-#[clap(author, version, about, long_about = None)]
-#[clap(name = "nit")]
-#[clap(author = "Hisam Fahri <me@hisamafahri.com>")]
-#[clap(version = "0.1.0")]
-#[clap(about = "nit helps you wrap your git commands")]
-#[clap(long_about = "Wrap your git commands with nit to make your workflow faster and more consistent")]
-#[clap(propagate_version = true)]
-struct Cli {
-    #[clap(subcommand)]
-    command: Commands,
-}
-
-#[derive(Subcommand)]
-enum Commands {
-    /// Commit staged changes on the current working directory
-    Commit(Commit),
-}
-
-#[derive(Parser)]
-struct Commit {
-    /// Commit all changes (staged & unstaged changes) on the current working directory
-    #[clap(short, long)]
-    all: bool, 
-}
+use clap::{Parser};
+mod cli;
 
 fn main() {
-    let cli = Cli::parse();
+    let cli = cli::Cli::parse();
 
     match &cli.command {
-        Commands::Commit(all) => commit_all(all)
+        cli::Commands::Commit(all) => commit_all(all)
     }
 
 }
 
-fn commit_all(commit: &Commit) {
+fn commit_all(commit: &cli::Commit) {
     println!("all: {:?}", commit.all);
     println!("commit all files");
 }
