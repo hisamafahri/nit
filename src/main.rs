00ac68339c9a2222f0fe7be6eb94332409e17d8e
Hisam Fahri <me@hisamafahri.com>
2022-04-24 22:57:45 +0700

    feat(project): add args input handler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+#[clap(propagate_version = true)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Commit staged changes on the current working directory
+    Commit(Commit),
+}
+
+#[derive(Parser)]
+struct Commit {
+    /// Commit all changes (staged & unstaged changes) on the current working directory
+    #[clap(short, long)]
+    all: bool, 
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Commit(all) => commit_all(all)
+    }
+
+}
+
+fn commit_all(commit: &Commit) {
+    println!("all: {:?}", commit.all);
+    println!("commit all files");
 }
