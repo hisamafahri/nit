5d56f776d4bae9c197a6f8c51e013a64de2c4ccd
Hisam Fahri <me@hisamafahri.com>
2022-04-24 23:03:13 +0700

    feat(main): add cli metadata

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,11 @@ use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
 #[clap(author, version, about, long_about = None)]
+#[clap(name = "nit")]
+#[clap(author = "Hisam Fahri <me@hisamafahri.com>")]
+#[clap(version = "0.1.0")]
+#[clap(about = "nit helps you wrap your git commands")]
+#[clap(long_about = "Wrap your git commands with nit to make your workflow faster and more consistent")]
 #[clap(propagate_version = true)]
 struct Cli {
     #[clap(subcommand)]
