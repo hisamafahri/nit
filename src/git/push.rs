16e9b2cea9532900630ea56776255935734fa4fc
Hisam Fahri <me@hisamafahri.com>
2022-04-26 14:31:09 +0700

    fix(push): add future async

--- src/git/push.rs
+++ src/git/push.rs
@@ -1,16 +1,18 @@
-use crate::helper;
+use std::process;
 use futures_lite::future;
 
 pub fn push(remote: &String, branch: &String) {
     println!("\x1B[7m STATUS: \x1B[0m pushing changes...");
-
-    let args = [
-        String::from("push"),
-        String::from(remote),
-        String::from(branch),
-    ];
     future::block_on(async {
-        let output = helper::command::run(&String::from("git"), &args);
-        helper::output::print(&output);
+        let cmd = process::Command::new("git")
+            .args(["push", &remote.trim(), &branch.trim()])
+            .stderr(std::process::Stdio::piped())
+            .stdout(std::process::Stdio::piped())
+            .stdin(std::process::Stdio::null())
+            .output()
+            .expect("\x1B[41;1m\x1B[37;1m ERROR: \x1B[0m\x1B[0m failed to run command");
+
+        println!("{}", String::from_utf8_lossy(&cmd.stderr).trim());
+        println!("{}", String::from_utf8_lossy(&cmd.stdout).trim());
     });
 }
