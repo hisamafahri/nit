48dab0655e123ebf3b2d6a00930be1d3de9525bc
Hisam Fahri <me@hisamafahri.com>
2022-04-30 23:14:59 +0700

    feat(clone): add clone handler

--- src/clone.rs
+++ src/clone.rs
@@ -1,3 +1,18 @@
+use crate::git;
+use crate::helper;
+use std::process;
+use url::Url;
+
 pub fn clone() {
-    println!("Clone command is called")   
+    let repo_url = helper::prompt::build::input(&String::from("Repository URL?"));
+
+    let parsed = Url::parse(&repo_url);
+
+    // If inputted url is not a valid url
+    if parsed.is_err() {
+        println!("\x1B[38;5;1m error \x1B[0m value is not a valid url!");
+        process::exit(1)
+    }
+
+    git::clone::clone(&repo_url);
 }
